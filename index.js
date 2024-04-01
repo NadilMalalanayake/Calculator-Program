a541b29aa2b6b5d453d73e51215aa8e2be839569
Nadil Malalanayake <nadilviksura@gmail.com>
2024-04-01 19:41:59 +0530

    upgraded some key buttons

---
+++ index.js
@@ -0,0 +1,19 @@
+const display=document.querySelector(".display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+appendToDisplay()
+
+function calculate(){
+
+}
+
+function clearDisplay(){
+
+}
+
+function deletValue(){
+    
+}
